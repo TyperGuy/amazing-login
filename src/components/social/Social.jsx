08e76868e7062bb1a938f22905e463e6a99bfca5
RJ Tony <ray.developper@gmail.com>
2021-08-12 00:54:28 +0100

    Setup Social Media Component

---
+++ src/components/social/Social.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Social.css';
+
+function Social(){
+  return(
+    <div>Simple Div</div>
+  );
+}
+export default Social;
---
+++ src/components/social/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import Social from './Social';
+export default Social;
