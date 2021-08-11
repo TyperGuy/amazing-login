646e69b470df0353d9c095fda9f006efd5a6d6c5
RJ Tony <ray.developper@gmail.com>
2021-08-12 00:48:42 +0100

    Setup Signup Component

---
+++ src/components/sign-up/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Signup.css';
+
+function Signup(){
+  return(
+    <div>Simple Div</div>
+  );
+}
+
+export default Signup;
---
+++ src/components/sign-up/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import Signup from './Signup';
+export default Signup;
