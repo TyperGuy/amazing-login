ce06bb0a483f4c646837fc67627976e4f63772df
RJ Tony <ray.developper@gmail.com>
2021-08-12 23:59:06 +0100

    Styles Siguin Form|Social Container

--- src/components/sign-up/Signup.jsx
+++ src/components/sign-up/Signup.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Signup.css';
+import Social from '../social/Social';
 
 function Signup(){
   return(
-    <div>Simple Div</div>
+    <form action="" className="sign-in-form">
+      <h2 className="title">Sign in</h2>
+      <div className="input-field">
+        <i className="fas fa-user"></i>
+        <input type="text" placeholder="UserName" />
+      </div>
+      <div className="input-field">
+        <i className="fas fa-lock"></i>
+        <input type="password" placeholder="PassWord" />
+      </div>
+      <input type="submit" className="btn solid" />
+      <Social/>
+
+    </form>
   );
 }
 
