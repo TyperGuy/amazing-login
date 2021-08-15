2fbafb4a49bd0877d6a3d1fefd6dcfba6c9585f5
RJ Tony <ray.developper@gmail.com>
2021-08-15 01:16:15 +0100

    Setup Sign-Up component

--- src/components/sign-up/Signup.jsx
+++ src/components/sign-up/Signup.jsx
@@ -4,14 +4,14 @@ import Social from '../social/Social';
 
 function Signup(){
   return(
-    <form action="" className="sign-in-form">
+    <form action="" className="sign-up-form">
       <h2 className="title">Sign Up</h2>
       <div className="input-field">
         <i className="fas fa-user"></i>
         <input type="text" placeholder="UserName" />
       </div>
       <div className="input-field">
-        <i className="fas fa-email"></i>
+        <i className="fas fa-envelope"></i>
         <input type="text" placeholder="Email" />
       </div>
       <div className="input-field">
